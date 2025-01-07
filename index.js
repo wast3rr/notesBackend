496ce0fc484bd7686687095f2fd1414c9fb78328
Karen <95060071+wast3rr@users.noreply.github.com>
2025-01-06 19:28:22 -0500

    More mongoDB stuff

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
+const Note = require('./models/note')
+
 const app = express()
 
 app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 
-let notes = [
-    {
-      id: "1",
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: "2",
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: "3",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-]
+const url = process.env.MONGODB_URI
 
 app.get('/',  (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -78,7 +67,7 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log('Server running on port ', PORT)
 })
