0cadcdf02c5adb4618af8d806fc686a6a0b9d006
Karen <95060071+wast3rr@users.noreply.github.com>
2025-01-11 20:19:37 -0500

    Added input validation

--- index.js
+++ index.js
@@ -31,34 +31,24 @@ app.get('/api/notes/:id', (request, respons, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
 
     const note = new Note({
         content: body.content,
-        important: Boolean(body.important) || false,
+        important: body.important || false,
     })
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
-
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
-
-    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+    const {content, important} = request.body
+    Note.findByIdAndUpdate(request.params.id, {content, important}, {new: true, runValidators: true, context: 'query'})
         .then(updatedNote => {
             response.json(updatedNote)
         })
@@ -89,6 +79,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
