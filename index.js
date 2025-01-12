2f2b9f122feba50984d5bdf1d0d28a1fa4f33dcb
Karen <95060071+wast3rr@users.noreply.github.com>
2025-01-11 20:01:09 -0500

    Added put/delete functionality to backend

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 
-const url = process.env.MONGODB_URI
-
 app.get('/',  (request, response) => {
 
 })
@@ -21,15 +19,16 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note)
+app.get('/api/notes/:id', (request, respons, next) => {
+    Note.findById(request.params.id)
+    .then(note => {
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
     })
-    .catch(error => {
-        return response.status(400).json({
-            error: 'id does not exist'
-        })
-    })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -51,14 +50,48 @@ app.post('/api/notes', (request, response) => {
     })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
 
-    response.status(204).end()
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log('Server running on port ', PORT)
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
