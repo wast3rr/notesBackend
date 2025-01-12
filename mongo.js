15cbe31e64b5120e5ad82215a81ae1746e2dbebe
Karen <95060071+wast3rr@users.noreply.github.com>
2025-01-11 20:49:05 -0500

    Added linting

--- mongo.js
+++ mongo.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url = 
+const url =
     'mongodb+srv://midasg91:' + password  + '@cluster0.2pbzp.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0'
 
 mongoose.set('strictQuery', false)
@@ -15,8 +15,8 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+  content: String,
+  important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
@@ -32,8 +32,8 @@ const Note = mongoose.model('Note', noteSchema)
 // })
 
 Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
 })
